8ec6e4a425b3b344e805559200641d3eade70924
Sam de Jong <samdejong86@gmail.com>
2024-09-20 09:58:13 -0700

    Moved some files around

--- scripts/updateSHA.sh
+++ scripts/updateSHA.sh
@@ -1,7 +1,8 @@
+# This script is called by updateSHA.tcl, which is called by Quartus at the start of compilation.
+# It updates the constant "GIT_SHA" to match the most recent git sha
 
-
+# Get the most recent SHA
 SHA=$(git rev-parse --short HEAD)
 
-echo $SHA
-
+# Replace the appropriate line in the V1495_regs_pkg file
 sed -i "/GIT_SHA/c\  constant GIT_SHA : std_logic_vector(31 downto 0) := x\"0${SHA}\";" src/V1495_regs_pkg.vhd
--- scripts/updateSHA.tcl
+++ scripts/updateSHA.tcl
@@ -1,3 +1,4 @@
+# This script is called by quartus before compilation
 post_message -type info "Updating GIT SHA"
 
 exec bash scripts/updateSHA.sh
